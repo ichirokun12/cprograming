738682c488621104c6818787beb8680c0979607e
ichirokun12 <135345654+ichirokun12@users.noreply.github.com>
2023-11-18 12:56:53 +0530

    Update Doubly_linkedList.c

--- Doubly_linkedList.c
+++ Doubly_linkedList.c
@@ -1,14 +1,16 @@
-# include <stdio.h>
-# include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node {
-    struct node* prev;
+struct node
+{
+    struct node *prev;
     int data;
-    struct node* next;
+    struct node *next;
 };
 
-struct node* addToEmpty(struct node* head , int data) {
-    struct node* temp = malloc(sizeof(struct node));
+struct node *AddToEmpty(struct node *head, int data)
+{
+    struct node *temp = malloc(sizeof(struct node));
     temp->prev = NULL;
     temp->data = data;
     temp->next = NULL;
@@ -16,8 +18,9 @@ struct node* addToEmpty(struct node* head , int data) {
     return head;
 }
 
-struct node* addAtBag(struct node* head, int data) {
-    struct node* temp = malloc(sizeof(struct node));
+struct node *AddToBag(struct node *head, int data)
+{
+    struct node *temp = malloc(sizeof(struct node));
     temp->prev = NULL;
     temp->data = data;
     temp->next = NULL;
@@ -27,14 +30,16 @@ struct node* addAtBag(struct node* head, int data) {
     return head;
 }
 
-struct node* AddAtEnd(struct node* head, int data) {
-    struct node* temp, *tp;
+struct node *AtTheEnd(struct node *head, int data)
+{
+    struct node *temp, *tp;
     temp = malloc(sizeof(struct node));
     temp->prev = NULL;
     temp->data = data;
     temp->next = NULL;
     tp = head;
-    while(tp->next != NULL) {
+    while (tp->next != NULL)
+    {
         tp = tp->next;
         tp->next = temp;
         temp->prev = tp;
@@ -42,19 +47,50 @@ struct node* AddAtEnd(struct node* head, int data) {
     }
 }
 
-int main() {
-    struct node* head = NULL;
-    struct node* ptr;
-     
-     head = addToEmpty(head, 20);
-     head = addAtBag(head, 30);
-     head = AddAtEnd(head, 40);
+struct node *AddAfterpos(struct node *head, int data, int position)
+{
+    struct node *newp = NULL;
+    struct node *temp = head;
+    struct node *temp2 = NULL;
+    newp = AddToEmpty(head, data);
 
-     ptr = head;
-     while(ptr != NULL) {
+    while (position != 1)
+    {
+
+        temp = temp->next;
+        position--;
+    }
+    if (temp == NULL)
+    {
+        temp->next = newp;
+        newp->prev = temp;
+    }
+    else
+    {
+        temp2 = temp->next;
+        temp->next = newp;
+        temp2->prev = newp;
+        newp->next = temp2;
+        newp->prev = temp;
+    }
+    return head;
+}
+
+int main()
+{
+    struct node *head = NULL;
+    struct node *ptr;
+
+    head = AddToEmpty(head, 20);
+    head = AddToBag(head, 100);
+    head = AtTheEnd(head, 10000);
+    head = AddAfterpos(head, 5, 2);
+
+    ptr = head;
+    while (ptr != NULL)
+    {
         printf("%d ", ptr->data);
-        ptr = ptr->next;  
-     }
-
-     return 0;
+        ptr = ptr->next;
+    }
+    return 0;
 }
