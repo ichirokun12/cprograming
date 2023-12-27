de2e11b42bb0c15c1137c5233fd0102043b91729
ichiro <135345654+ichirokun12@users.noreply.github.com>
2023-12-27 13:02:35 +0530

    Create binary_search_tree.c

---
+++ binary_search_tree.c
@@ -0,0 +1,64 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+
+};
+
+struct node * createnode(int data) {
+    struct node *n;
+    n = (struct node *) malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+    
+}
+
+void inorder(struct node *root) {
+    if(root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data );
+        inorder(root->right);
+    }
+}
+
+
+    int isBST(struct node *root) {
+        struct node *prev = NULL;
+        if(root != NULL) {
+            if(!isBST(root->left)) {
+                return 0;
+            }
+            if(prev != NULL && root -> data <= prev->data) {
+                return 0;
+            }
+            prev = root;
+            return isBST(root -> right);
+        } else {
+            return 1;
+        }
+    }
+
+    int main() {
+
+        struct node *p = createnode(1);
+        struct node *p1 = createnode(2);
+        struct node *p2 = createnode(4);
+        struct node *p3 = createnode(8);
+        struct node *p4 = createnode(16);
+
+        p->left = p1;
+        p->right = p2;
+        p1->left = p3;
+        p1->right = p4;
+
+        inorder(p);
+        printf("\n");
+        printf("%d",isBST(p));
+
+        return 0;
+    }
