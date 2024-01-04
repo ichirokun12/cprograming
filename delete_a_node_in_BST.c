64af4f3430ca45923a945a9f7240e7d5eb7dd95c
ichiro <135345654+ichirokun12@users.noreply.github.com>
2024-01-04 14:21:41 +0530

    Create delete_a_node_in_BST.c

---
+++ delete_a_node_in_BST.c
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createnode(int data)
+{
+    struct node *n;
+    n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void preorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void inorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int isBST(struct node *root)
+{
+    static struct node *prev = NULL;
+
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+        {
+            return 0;
+        }
+
+        if (prev != NULL && root->data <= prev->data)
+        {
+            return 0;
+        }
+
+        prev = root;
+        return isBST(root->right);
+    }
+
+    else
+    {
+        return 1;
+    }
+}
+
+struct node *search(struct node *root, int key)
+{
+    while (root != NULL)
+    {
+        if (key == root->data)
+        {
+            return root;
+        }
+
+        else if (key < root->data)
+        {
+            root = root->left;
+        }
+
+        else
+        {
+            root = root->right;
+        }
+    }
+    return NULL;
+}
+
+void insert(struct node *root, int key)
+{
+    struct node *prev = NULL;
+    while (root != NULL)
+    {
+        prev = root;
+        if (key == root->data)
+        {
+            return;
+        }
+
+        else if (key < root->data)
+        {
+            root = root->left;
+        }
+
+        else
+        {
+            root = root->right;
+        }
+    }
+    struct node *new = createnode(key);
+    if (key < prev->data)
+    {
+        prev->left = new;
+    }
+    else
+    {
+        prev->right = new;
+    }
+}
+struct node *inorderPredecessor(struct node *root)
+{
+    root = root->left;
+    while (root->right != NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
+struct node *deleteNode(struct node *root, int value)
+{
+    struct node *ipre;
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        free(root);
+        return NULL;
+    }
+    if (value < root->data)
+    {
+        root->left = deleteNode(root->left, value);
+    }
+    else if (value > root->data)  // Corrected condition here
+    {
+        root->right = deleteNode(root->right, value);
+    }
+    else
+    {
+        ipre = inorderPredecessor(root);
+        root->data = ipre->data;
+        root->left = deleteNode(root->left, ipre->data);
+    }
+    return root;
+}
+
+
+int main()
+{
+    struct node *p = createnode(8);
+    struct node *p1 = createnode(6);
+    struct node *p2 = createnode(10);
+    struct node *p3 = createnode(4);
+    struct node *p4 = createnode(7);
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    // struct node *n = search(p, 12);
+    // if (n != NULL)
+    // {
+    //     printf("Found %d\n", n->data);
+    // }
+    // else
+    // {
+    //     printf("Element is not found\n");
+    // }
+    // insert(p, 12);
+
+    inorder(p);
+    deleteNode(p, 10);
+    printf("\n");
+    inorder(p);
+
+
+    return 0;
+} 
